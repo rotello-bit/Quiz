93aa850a611dbf8c2780d00b9a4fbb60a7a8bad4
Cihat <cihat@menufay.com>
2021-12-18 22:25:10 +0300

    new commit

--- quiz/models.py
+++ quiz/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
 
-class QuestionModel(models.Model):
-    question = models.CharField(max_length=250, null=True)
-    option1 = models.CharField(max_length=250, null=True)
-    option2 = models.CharField(max_length=250, null=True)
-    option3 = models.CharField(max_length=250, null=True)
-    option4 = models.CharField(max_length=250, null=True)
-    answer = models.CharField(max_length=250, null=True)
+class Quiz(models.Model):
+    name = models.CharField(max_length=250, null=True)
 
     def __str__(self):
-        return self.question
+        return self.name
 
+
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    quiz = models.ForeignKey(Quiz, db_column='quiz_id', on_delete=models.CASCADE)
+    no = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=250, null=True)
+
+    class Meta:
+        unique_together = ('id', 'no',)
+
+    def __str__(self):
+        return self.name
+
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, db_column='question_id', on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, db_column='question_id', on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, db_column='option_id', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Soru: ' + self.question.name + '  Cevap: ' + self.option.name
