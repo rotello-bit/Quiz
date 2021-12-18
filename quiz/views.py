039d688442889fdc6ac6572ed77266ff32b8be15
rotello-bit <76389531+rotello-bit@users.noreply.github.com>
2021-12-18 22:05:35 +0300

    s
    s

--- quiz/views.py
+++ quiz/views.py
@@ -3,12 +3,24 @@ from quiz.models import *
 
 
 def home(request, pk):
+    context = {}
+    return render(request, 'quiz.html', context)
 
+
+def get_quizzes(request):
     context = {
-        'questions': QuestionModel.objects.filter(id=pk),
+        'quizzes': Quiz.objects.all().values('id', 'name')
     }
 
     return render(request, 'quiz.html', context)
 
 
+def get_question(request, quiz_id, question_no):
+    question = Question.objects.filter(quiz_id=quiz_id, no=question_no).values('id', 'name')
+    options = Option.objects.filter(question_id=question.id).valuse('id', 'name')
+    context = {
+        "question": question,
+        "options": options
+    }
 
+    return render(request, 'quiz.html', context)
